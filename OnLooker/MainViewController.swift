8bbb669fcd7bdbc85b305b3d887f02462213de06
Jal Irani <jal@Jals-MacBook-Pro.local>
2019-08-13 15:51:12 -0400

    init commit

---
+++ OnLooker/MainViewController.swift
@@ -0,0 +1,52 @@
+//
+//  MainViewController.swift
+//  OnLooker
+//
+//  Created by Jal Irani on 8/8/19.
+//  Copyright © 2019 Jal Irani. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var videoTableView: UITableView!
+    var arr = [1,2,3,4,5,6]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        videoTableView.delegate = self
+        videoTableView.dataSource = self
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension MainViewController {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arr.count
+    }
+}
+
+extension MainViewController {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "subVideoCell", for: indexPath) as! UITableViewCell
+        cell.textLabel?.text = String(arr[indexPath.row])
+        return cell
+    }
+}
